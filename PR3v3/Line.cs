1bf74eca1795de9608116c25a9bbcb2c02bc19f1
deltaDelete <samansy@yandex.ru>
2023-01-18 21:09:04 +0700

    WIP: пересчет координат

--- PR3v3/Line.cs
+++ PR3v3/Line.cs
@@ -6,41 +6,85 @@ using System.Threading.Tasks;
 
 namespace PR3v3;
 public class Line {
-    private (float x, float y) _point1;
-    private (float x, float y) _point2;
-    private float _length;
+    #region Fields
 
-    public (float x, float y) Point1 {
+    private (int x, int y) _point1;
+    private (int x, int y) _point2;
+    private double _length;
+
+    #endregion
+
+    #region Properties
+
+    public (int x, int y) Point1 {
         get => _point1;
         set {
-            //_point1 = value;
-            var newLength = MathF.Sqrt(
-                MathF.Pow(Point2.x - Point1.x, 2)
-                + MathF.Pow(Point2.y - Point1.y, 2)
+            var newLength = Math.Sqrt(
+                Math.Pow(Point2.x - value.x, 2)
+                + Math.Pow(Point2.y - value.y, 2)
             );
-            if (newLength != _length) {
-                var fi = MathF.Atan(
-                    value.y - 
-                );
+            if (Math.Abs(newLength - _length) > 0.0001f) {
+                _point1 = value;
+                _point2 = GetNewPoint(value, _point2);
             }
         }
     }
-    public (float x, float y) Point2 {
+    public (int x, int y) Point2 {
         get => _point2;
         set {
-            _point2 = value;
+            var newLength = Math.Sqrt(
+                Math.Pow(value.x - Point1.x, 2)
+                + Math.Pow(value.y - Point1.y, 2)
+            );
+            if (Math.Abs(newLength - _length) > 0.0001f) {
+                _point1 = GetNewPoint(value, _point1);
+                _point2 = value;
+            }
         }
     }
 
-    public Line((float x, float y) point1, (float x, float y) point2) {
+    #endregion
+
+    #region Constructors
+
+    public Line((int x, int y) point1, (int x, int y) point2) {
         _point1 = point1;
         _point2 = point2;
 
-        _length = MathF.Sqrt(
-            MathF.Pow(Point2.x - Point1.x, 2)
-            + MathF.Pow(Point2.y - Point1.y, 2)
+        _length = Math.Sqrt(
+            Math.Pow(Point2.x - Point1.x, 2)
+            + Math.Pow(Point2.y - Point1.y, 2)
         );
     }
 
-    public Line(float x1, float y1, float x2, float y2) : this((x1, y1), (x2, y2)) { }
+    public Line(int x1, int y1, int x2, int y2) : this((x1, y1), (x2, y2)) { }
+
+    #endregion
+
+    #region Private methods
+
+    private (int x, int y) GetNewPoint((int x, int y) newPoint, (int x, int y) secondPoint) {
+        
+        var fi = Math.Atan(
+            (newPoint.y - secondPoint.y) / (newPoint.x - secondPoint.x)
+        );
+
+        int x22 = (int)Math.Round(_length * Math.Cos(fi) + newPoint.x, MidpointRounding.AwayFromZero);
+        int y22 = (int)Math.Round(_length * Math.Cos(fi) + newPoint.y, MidpointRounding.AwayFromZero);
+
+        return (x22, y22);
+    }
+
+    #endregion
+
+    #region Public methods
+    
+    public double CalcLength() {
+        return Math.Sqrt(
+            Math.Pow(this._point2.x - this._point1.x, 2)
+            + Math.Pow(this._point2.y - this._point1.y, 2)
+        );
+    }
+
+    #endregion
 }
--- PR3v3/Program.cs
+++ PR3v3/Program.cs
@@ -2,12 +2,28 @@
 
 using PR3v3;
 
-Console.BackgroundColor = ConsoleColor.Green;
-Console.Clear();
-
+// TODO: fix баг длина изменяется при изменение координат точки (x,y) не на одинаковое число
 var line1 = new Line(1,2,3,4);
 var line2 = new Line(3,4,1,2);
 
-Console.WriteLine($"Значение первой линии:\n\tX1: {line1.Point1.x}\n\tY1: {line1.Point1.y}\n\tX2: {line1.Point2.x}\n\tY1: {line1.Point2.y}");
-Console.WriteLine($"Значение второй линии:\n\tX1: {line2.Point1.x}\n\tY1: {line2.Point1.y}\n\tX2: {line2.Point2.x}\n\tY1: {line2.Point2.y}");
+Console.WriteLine($"Значение первой линии:\n\tX1: {line1.Point1.x}\n\tY1: {line1.Point1.y}\n\tX2: {line1.Point2.x}\n\tY2: {line1.Point2.y}");
+Console.WriteLine($"Значение второй линии:\n\tX1: {line2.Point1.x}\n\tY1: {line2.Point1.y}\n\tX2: {line2.Point2.x}\n\tY2: {line2.Point2.y}");
 
+var length11 = line1.CalcLength();
+var length21 = line2.CalcLength();
+
+line1.Point1 = (2, 3);
+line2.Point2 = (1, 4);
+
+Console.WriteLine($"Значение первой линии:\n\tX1: {line1.Point1.x}\n\tY1: {line1.Point1.y}\n\tX2: {line1.Point2.x}\n\tY2: {line1.Point2.y}");
+Console.WriteLine($"Значение второй линии:\n\tX1: {line2.Point1.x}\n\tY1: {line2.Point1.y}\n\tX2: {line2.Point2.x}\n\tY2: {line2.Point2.y}");
+
+var length12 = line1.CalcLength();
+var length22 = line2.CalcLength();
+
+Console.WriteLine($"Длины:\n\t11: {length11}" +
+                  $"\n\t12: {length12}" +
+                  $"\n\t21: {length21}" +
+                  $"\n\t22: {length22}");
+
+Console.ReadKey();
