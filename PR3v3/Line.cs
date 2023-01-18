8685f6f31af05268c9cd03909daaef87b7a038dc
Воронин Роман Витальевич гр. ИС(ПРО)-21 <is13088@stud.KHPK.RU>
2023-01-18 15:12:35 +0700

    initial commit

---
+++ PR3v3/Line.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR3v3;
+public class Line {
+    private (float x, float y) _point1;
+    private (float x, float y) _point2;
+    private float _length;
+
+    public (float x, float y) Point1 {
+        get => _point1;
+        set {
+            //_point1 = value;
+            var newLength = MathF.Sqrt(
+                MathF.Pow(Point2.x - Point1.x, 2)
+                + MathF.Pow(Point2.y - Point1.y, 2)
+            );
+            if (newLength != _length) {
+                var fi = MathF.Atan(
+                    value.y - 
+                );
+            }
+        }
+    }
+    public (float x, float y) Point2 {
+        get => _point2;
+        set {
+            _point2 = value;
+        }
+    }
+
+    public Line((float x, float y) point1, (float x, float y) point2) {
+        _point1 = point1;
+        _point2 = point2;
+
+        _length = MathF.Sqrt(
+            MathF.Pow(Point2.x - Point1.x, 2)
+            + MathF.Pow(Point2.y - Point1.y, 2)
+        );
+    }
+
+    public Line(float x1, float y1, float x2, float y2) : this((x1, y1), (x2, y2)) { }
+}
---
+++ PR3v3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Отступ для красоты
+
+using PR3v3;
+
+Console.BackgroundColor = ConsoleColor.Green;
+Console.Clear();
+
+var line1 = new Line(1,2,3,4);
+var line2 = new Line(3,4,1,2);
+
+Console.WriteLine($"Значение первой линии:\n\tX1: {line1.Point1.x}\n\tY1: {line1.Point1.y}\n\tX2: {line1.Point2.x}\n\tY1: {line1.Point2.y}");
+Console.WriteLine($"Значение второй линии:\n\tX1: {line2.Point1.x}\n\tY1: {line2.Point1.y}\n\tX2: {line2.Point2.x}\n\tY1: {line2.Point2.y}");
+
